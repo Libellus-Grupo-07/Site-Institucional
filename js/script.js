3ce81fcfc7299894cdb435ead7e26ece5a0be884
jessicabarreirosm <jessica.martins@sptech.school>
2024-03-30 18:53:24 -0300

    Inserção de responssivdade

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let aberto = false;
+let a = document.querySelectorAll("a");
+
+function mostrarMenu() {
+    let navbar;
+
+    if (aberto) {
+        aberto = false;
+        navbar = document.querySelector(".slow");
+        navbar.classList.remove("slow");
+        navbar.classList.add("nav-item");
+    } else {
+        aberto = true;
+        navbar = document.querySelector(".nav-item");
+        navbar.classList.remove("nav-item");
+        navbar.classList.add("slow");
+    }
+
+}   
+
+addEventListener(click, )
